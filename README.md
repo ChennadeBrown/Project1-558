b258e3f95107a30ac0fbe23b4dce82ec2d3cd1c4
Chennade Brown <cjbrow25@ncsu.edu>
2021-09-27 13:24:37 -0400

    commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Project 1-API-Vignette
+================
+Chennade Brown
+9/26/2021
+
+\#Practice Today is going good. Today.
---
+++ READMERScript.R
@@ -0,0 +1,8 @@
+
+rmarkdown:: render("Project 1.Rmd",
+                   output_format = "github_document",
+                   output_file = "README.md",
+                   output_options = list(html_preview = FALSE, keep_html = FALSE)
+)
+
+
