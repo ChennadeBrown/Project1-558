52b86bbe15283d8fd2226812734c80b3fc4950ec
Chennade Brown <cjbrow25@ncsu.edu>
2021-10-01 18:24:42 -0400

    commit # ?

--- READMERScript.R
+++ READMERScript.R
@@ -2,7 +2,7 @@
 rmarkdown:: render("Project 1.Rmd",
                    output_format = "github_document",
                    output_file = "README.md",
-                   output_options = list(html_preview = FALSE, keep_html = FALSE, toc = TRUE, toc_depth = "2", df_print = "tibble"))
+                   output_options = list(html_preview = FALSE, keep_html = FALSE, toc = TRUE, toc_depth = "1", df_print = "tibble"))
 
 
 
